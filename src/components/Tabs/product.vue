a2efab0dbbfead00ad9f345dbb66230a4faa48b1
GuoXiaoyi <guoxiaoyi888@gmail.com>
2022-08-24 11:54:05 +0800

    产品列表

--- src/components/Tabs/product.vue
+++ src/components/Tabs/product.vue
@@ -10,7 +10,10 @@
       <router-link :to="{name: 'ProductShowMaterials', params: {id: $route.params.id }}">产品原材料</router-link>
     </li>
     <li :class="{'active': $route.name === 'ProductShowBatches'}">
-      <router-link :to="{name: 'ProductShowBatches', params: {id: $route.params.id }}">产品批次管理</router-link>
+      <!-- <router-link :to="{name: 'ProductShowBatches', params: {id: $route.params.id }}">产品批次管理</router-link> -->
+      <a :href="'/admin/products/'+ $route.params.id +'/product_batches'">
+        产品批次管理
+      </a>
     </li>
     <li :class="{'active': $route.name === 'ProductShowTUnitSpecs'}">
       <router-link :to="{name: 'ProductShowTUnitSpecs', params: {id: $route.params.id }}">包装规格管理</router-link>
