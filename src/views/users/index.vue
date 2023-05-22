495ad541af702d6613f615a86fad492467fbb3b3
GuoXiaoyi <guoxiaoyi888@gmail.com>
2023-05-22 18:18:06 +0800

    用户管理

---
+++ src/views/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  activated() {
+    this.$store.dispatch('breadcrumb/set_breadcrumb', [
+      { title: '用户管理' }
+    ])
+  }
+}
+</script>
+
+<style>
+
+</style>
