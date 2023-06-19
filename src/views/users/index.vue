868e55624d131f0f1a7a0c06f3eceb6e0cd8d092
GuoxiaoYi <1072992047@qq.com>
2023-06-19 16:00:09 +0800

    用户管理

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -7,11 +7,8 @@
       <div class="panel-body">
         <div class="page_toolbar search_toolbar">
           <el-form ref="filterForm" :inline="true" size="small" class="filter-form-inline">
-            <el-form-item label="昵称">
-              <el-input v-model="query.nickname" placeholder="昵称" />
-            </el-form-item>
-            <el-form-item label="手机号">
-              <el-input v-model="query.phone" placeholder="手机号" />
+            <el-form-item label="搜索">
+              <el-input v-model="query.blurry" placeholder="昵称/姓名/手机号" />
             </el-form-item>
             <el-form-item label="性别">
               <el-select v-model="query.gender" clearable>
