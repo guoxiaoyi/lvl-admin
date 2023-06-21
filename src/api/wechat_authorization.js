06e6217d47deb25966a9ba18a9bdf087474649d6
GuoxiaoYi <1072992047@qq.com>
2023-06-21 15:38:35 +0800

    公众号

---
+++ src/api/wechat_authorization.js
@@ -0,0 +1,17 @@
+import request from '@/utils/request'
+
+export function show() {
+  return request({
+    url: '/lmp/v2/admin/wechat_profile',
+    method: 'get'
+  })
+}
+
+export function pre_auth_url() {
+  return request({
+    url: '/lmp/v2/admin/wechat_profile/pre_auth_url',
+    method: 'get'
+  })
+}
+
+export default { show, pre_auth_url }
