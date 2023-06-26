e5ed00ccd4732c9ec7ac900559168eae2754f04e
GuoxiaoYi <1072992047@qq.com>
2023-06-26 22:28:13 +0800

    积分记录导航

--- src/views/users/points_trans.vue
+++ src/views/users/points_trans.vue
@@ -47,7 +47,7 @@ export default {
     })
     this.$store.dispatch('breadcrumb/set_breadcrumb', [
       { title: '用户管理', path: { name: 'UserIndex' }},
-      { title: this.detail.name || '用户详情', path: { name: 'UserShow', params: { userId: this.detail.id }}},
+      { title: '用户详情', path: { name: 'UserShow', params: { userId: this.detail.id }}},
       { title: '积分记录' }
     ])
     this.crud.refresh()
