e1bd39d785deda2c19c40455f41d1bd10466cc7b
GuoxiaoYi <1072992047@qq.com>
2023-06-27 18:59:38 +0800

    微信公众号修改

--- src/api/wechat_authorization.js
+++ src/api/wechat_authorization.js
@@ -14,4 +14,11 @@ export function pre_auth_url(url) {
   })
 }
 
-export default { show, pre_auth_url }
+export function authorize(authCode) {
+  return request({
+    url: `/lmp/v2/admin/wechat_profile/authorize/${authCode}`,
+    method: 'post'
+  })
+}
+
+export default { show, pre_auth_url, authorize }
--- src/views/wechat_authorization/callback.vue
+++ src/views/wechat_authorization/callback.vue
@@ -1,10 +1,62 @@
 <template>
-  <div class="app-container">123</div>
+  <div class="app-container">
+    <ul class="nav nav-tabs">
+      <li class="active">
+        <a aria-current="page" href="javascript:;">
+          绑定微信公众号
+        </a>
+      </li>
+    </ul>
+    <div v-loading="true" class="panel panel-default">
+      <div class="panel-body">
+        <div class="text-center">
+          <p><img :src="require('@/assets/wx_logo.png')" width="60" height="60"></p>
+          <p class="lead">
+            授权绑定微信公众号<br>把账户和微信打通，引流公众号建立私域流量。
+          </p>
+        </div>
+        <el-row :gutter="20">
+          <el-col :span="16" :offset="4">
+            <div class="alert alert-warning">
+              <h5><i class="fa fa-warning" /> 提示：</h5>
+              <ul>
+                <li>一个微信公众号只能和一个帐户绑定</li>
+                <li>为保证账户功能正常，授权时请保持默认选择</li>
+                <li>已认证的公众号，成功绑定后不可更换公众号</li>
+                <li>成功绑定公众号后，解绑会影响平台功能，请谨慎解绑</li>
+              </ul>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import wechat_authorization from '@/api/wechat_authorization'
 export default {
+  data() {
+    return {
 
+    }
+  },
+  mounted() {
+    this.$store.dispatch('breadcrumb/set_breadcrumb', [
+      { title: '绑定微信公众号' }
+    ])
+    console.log(this.$route.query)
+    if (this.$route.query.auth_code) {
+      wechat_authorization.authorize(this.$route.query.auth_code).then(response => {
+        this.$router.push({ name: 'WechatAuthorizationInfo' })
+      }).catch(fail => {
+        this.$router.push({ name: 'WechatAuthorizationAuthorize' })
+      })
+    }
+    if (!this.$route.query.auth_code) {
+      this.$router.push({ name: 'WechatAuthorizationAuthorize' })
+    }
+  }
 }
 </script>
 
