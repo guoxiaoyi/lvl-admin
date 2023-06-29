d0f53bc415ec11f288fb3355b46427e32df80868
GuoxiaoYi <1072992047@qq.com>
2023-06-29 19:47:11 +0800

    修改用户标签接口

--- src/api/tag.js
+++ src/api/tag.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 export function all(params) {
   return request({
-    url: '/lmp/v2/admin/tag/all',
+    url: '/lmp/v2/admin/tag/list',
     method: 'get',
     params
   })
