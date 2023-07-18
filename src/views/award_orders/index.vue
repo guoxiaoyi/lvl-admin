59d400bf95af570d4c388c75b4bd1c524bea0b7d
GuoxiaoYi <1072992047@qq.com>
2023-07-18 15:29:34 +0800

    兑奖订单

--- src/views/award_orders/index.vue
+++ src/views/award_orders/index.vue
@@ -333,7 +333,6 @@ export default {
     'export_data_status.state'() {
       if (this.export_data_status.state === 'finished') {
         clearInterval(this.set_interval_id)
-        window.location.reload()
       }
     },
     'export_data_modal.show'() {
