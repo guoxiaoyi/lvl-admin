67e293037656c97b44482996b4e80a087dce8a1a
GuoxiaoYi <1072992047@qq.com>
2023-08-01 16:03:38 +0800

    兑奖订单提示

--- src/views/award_orders/index.vue
+++ src/views/award_orders/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="app-container">
+    <div class="alert alert-info" role="alert">
+      <i class="fa fa-info-circle" /> 平台目前仅展示最近12个月的数据。如需查看或导出更久的历史数据，请联系客户经理。
+    </div>
     <AwardOrders />
   </div>
 </template>
