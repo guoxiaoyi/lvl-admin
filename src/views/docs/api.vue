a9a37ffe99016d2dbbfd0c38c47db82f9e1d41c1
GuoxiaoYi <1072992047@qq.com>
2023-12-12 14:09:49 +0800

    api 文档

---
+++ src/views/docs/api.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <vue-markdown :source="markdownContent" />
+  </div>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown';
+
+export default {
+  components: {
+    VueMarkdown,
+  },
+  data() {
+    return {
+      markdownContent: '',
+    };
+  },
+  mounted() {
+    this.loadMarkdownFile();
+  },
+  methods: {
+    async loadMarkdownFile() {
+      try {
+        const markdownFile = require('./api.md')
+        this.markdownContent = markdownFile.default || markdownFile;
+      } catch (error) {
+        console.error('Error loading Markdown file', error);
+      }
+    },
+  },
+};
+</script>
