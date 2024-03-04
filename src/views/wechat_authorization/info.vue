13055ef1a83c8f227aaf5749d893964ca902c0ca
GuoxiaoYi <1072992047@qq.com>
2024-03-04 16:23:44 +0800

    绑定公众号去除权限

--- src/views/wechat_authorization/info.vue
+++ src/views/wechat_authorization/info.vue
@@ -14,7 +14,7 @@
             <img :src="require('@/assets/wx_logo.png')" width="208">
             <div class="wechat-status">
               <div v-if="detail.authorized">
-                <el-button v-if="checkPer(['wechat_menu_manage'])" @click="$router.push({ name: 'WechatAuthorizationAuthorize' })">重新绑定公众号</el-button>
+                <el-button @click="$router.push({ name: 'WechatAuthorizationAuthorize' })">重新绑定公众号</el-button>
                 <dl style="margin-top: 30px;">
                   <dt>公众号名称:</dt>
                   <dd>{{ detail.nickName }}</dd>
@@ -32,7 +32,7 @@
                 </dl>
               </div>
               <div v-else>
-                <el-button v-if="checkPer(['wechat_menu_manage'])" @click="$router.push({ name: 'WechatAuthorizationAuthorize' })">立即绑定公众号</el-button>
+                <el-button @click="$router.push({ name: 'WechatAuthorizationAuthorize' })">立即绑定公众号</el-button>
               </div>
             </div>
           </div>
