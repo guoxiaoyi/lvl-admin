0af0d450d5242282f5a00c7c89762a8a307361a5
GuoxiaoYi <1072992047@qq.com>
2024-03-13 14:46:47 +0800

    用户零钱明细

--- vue.config.js
+++ vue.config.js
@@ -58,7 +58,7 @@ module.exports = {
         }
       }
     },
-    before: require('./mock/mock-server.js')
+    after: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
