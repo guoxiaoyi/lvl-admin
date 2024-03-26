4a3a83a4debbea22291ea7de5eb05edf3de6017d
GuoxiaoYi <1072992047@qq.com>
2024-03-26 17:28:20 +0800

    优化公众号菜单

--- src/views/wx_replies/index.vue
+++ src/views/wx_replies/index.vue
@@ -31,8 +31,8 @@
               </template>
             </el-table-column>
           </el-table>
-          <pagination />
         </div>
+        <pagination />
       </div>
     </div>
   </div>
