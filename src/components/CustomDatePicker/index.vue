2b7257093ae5b154c14341f19156ce1943a4a1e2
GuoxiaoYi <1072992047@qq.com>
2024-04-11 14:53:32 +0800

    时间组件优化

--- src/components/CustomDatePicker/index.vue
+++ src/components/CustomDatePicker/index.vue
@@ -1,15 +1,22 @@
-<!-- 为解决用户手输入时间，没有时间范围, 导致后台查询错误，添加默认时间 -->
 <template>
-  <el-date-picker
-    v-model="internalDateRange"
-    type="daterange"
-    start-placeholder="开始时间"
-    end-placeholder="结束时间"
-    placeholder="选择日期范围"
-    :clearable="clearable"
-    :picker-options="pickerOptions"
-    @change="handleChange"
-  />
+  <div class="el-custom-input-group">
+    <el-date-picker
+      v-model="startDate"
+      type="date"
+      placeholder="开始时间"
+      :clearable="clearable"
+      @change="handleStartChange"
+      @blur="handleStartBlur"
+    />
+    <div class="el-input-group-addon">至</div>
+    <el-date-picker
+      v-model="endDate"
+      type="date"
+      placeholder="结束时间"
+      :clearable="clearable"
+      @change="handleEndChange"
+    />
+  </div>
 </template>
 
 <script>
@@ -30,79 +37,57 @@ export default {
     },
     pickerOptions: {
       type: Object,
-      default: () => {
-        return {
-          shortcuts: [
-            {
-              text: '今天',
-              onClick(picker) {
-                picker.$emit('pick', [moment().format('YYYY-MM-DD 00:00:00'), moment().format('YYYY-MM-DD 23:59:59')])
-              }
-            },
-            {
-              text: '昨天',
-              onClick(picker) {
-                picker.$emit('pick', [moment().subtract(1, 'day').format('YYYY-MM-DD 00:00:00'), moment().subtract(1, 'day').format('YYYY-MM-DD 23:59:59')])
-              }
-            },
-            {
-              text: '最近7天',
-              onClick(picker) {
-                picker.$emit('pick', [moment().subtract(7, 'day').format('YYYY-MM-DD 00:00:00'), moment().format('YYYY-MM-DD 23:59:59')])
-              }
-            },
-            {
-              text: '最近30天',
-              onClick(picker) {
-                picker.$emit('pick', [moment().subtract(30, 'day').format('YYYY-MM-DD 00:00:00'), moment().format('YYYY-MM-DD 23:59:59')])
-              }
-            }
-          ]
-        }
-      }
+      default: () => {}
     }
   },
   data() {
     return {
-      internalDateRange: this.value
+      startDate: this.value.length ? this.value[0] : '',
+      endDate: this.value.length ? this.value[1] : ''
     }
   },
   watch: {
     value(newVal) {
-      this.internalDateRange = newVal
+      this.startDate = newVal.length ? newVal[0] : '';
+      this.endDate = newVal.length ? newVal[1] : '';
     }
   },
   methods: {
-    handleChange(value) {
-      if (Array.isArray(value) && value.length === 2) {
-        this.applyDefaultTime(value)
-      } else {
-        this.internalDateRange = value
-      }
-      this.$emit('input', this.internalDateRange)
+    handleStartChange(value) {
+      this.updateRange(value, this.endDate);
     },
-    applyDefaultTime(dateRange) {
-      let [startDate, endDate] = dateRange
+    handleStartBlur(data) {
+      console.log(123)
+    },
+    handleEndChange(value) {
+      this.updateRange(this.startDate, value);
+    },
+    updateRange(start, end) {
+      // 应用默认时间
+      const formattedStart = this.formatDate(start, this.defaultTime[0]);
+      const formattedEnd = this.formatDate(end, this.defaultTime[1]);
 
-      // 格式化日期并添加默认时间
-      startDate = this.formatDate(new Date(startDate), this.defaultTime[0])
-      endDate = this.formatDate(new Date(endDate), this.defaultTime[1])
-
-      this.internalDateRange = [startDate, endDate]
+      // 更新内部值并通知父组件
+      this.$emit('input', [formattedStart, formattedEnd]);
     },
     formatDate(date, time) {
-      const [hours, minutes, seconds] = time.split(':')
-      date.setHours(hours, minutes, seconds, 0)
-
-      const year = date.getFullYear()
-      let month = date.getMonth() + 1
-      let day = date.getDate()
-
-      month = month < 10 ? '0' + month : month
-      day = day < 10 ? '0' + day : day
-
-      return `${year}-${month}-${day} ${time}`
+      if (!date) return '';
+      const momentDate = moment(date).format('YYYY-MM-DD');
+      return `${momentDate} ${time}`;
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+::v-deep {
+  .el-input--prefix .el-input__inner {
+    padding-left: 15px;
+  }
+  .el-input__prefix {
+    display: none;
+  }
+}
+.el-custom-input-group {
+  width: 290px;
+}
+</style>
