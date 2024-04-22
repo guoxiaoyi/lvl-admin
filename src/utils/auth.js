ed6180f14cbfb1a4981a071b32536efb8ae7cb6e
GuoxiaoYi <1072992047@qq.com>
2024-04-22 21:51:42 +0800

    token续期问题

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -11,8 +11,8 @@ function checkAndRenewTokenIfNeeded() {
     const currentDate = new Date()
     const timeDiff = expiryDate.getTime() - currentDate.getTime()
     const dayDiff = timeDiff / (1000 * 60 * 60 * 24)
-    // 如果Token将在1天内过期，则续期4天
-    if (dayDiff <= 1) {
+    // 如果Token将在{{DefaultDays}}天内过期，则续期4天
+    if (dayDiff <= DefaultDays) {
       const tokenValue = Cookies.get(TokenKey) // 获取当前Token的值
       setToken(tokenValue) // 重新设置Token，续期4天
     }
