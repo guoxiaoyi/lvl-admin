3ad44dd828d8530a4585db5a2e80ae1f63a9d12f
GuoxiaoYi <1072992047@qq.com>
2024-06-11 22:52:54 +0800

    成套奖项隐藏中奖排除

--- src/views/activities/awards.vue
+++ src/views/activities/awards.vue
@@ -244,7 +244,7 @@
           </el-select>
           <p class="help-block">用户中奖后，会给该用户打上相应的标签，或<router-link :to="{ name: 'UserTags'}" target="_blank">新建用户标签</router-link></p>
         </el-form-item>
-        <el-form-item v-if="account.store.awardExcludeEnabled && form.type !== 'UserTagAward'" label="中奖排除">
+        <el-form-item v-if="account.store.awardExcludeEnabled && !['SuiteAward', 'UserTagAward'].includes(form.type)" label="中奖排除">
           <el-select v-model="form.excludeUserTagIds" multiple :multiple-limit="10" clearable>
             <el-option
               v-for="(item, index) in userTags"
