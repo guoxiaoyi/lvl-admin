da92f563b98523cd8534f26cff24ff0a091674d2
GuoxiaoYi <1072992047@qq.com>
2024-06-17 18:12:24 +0800

    步聚条样式修改

--- src/views/wizard/step.vue
+++ src/views/wizard/step.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-steps :active="active" finish-status="success" align-center process-status="finish">
+    <el-steps :active="active" finish-status="success" align-center>
       <el-step title="1.绑定公众号" />
       <el-step title="2.设置账户信息" />
       <el-step title="3.完成，等待审核" />
