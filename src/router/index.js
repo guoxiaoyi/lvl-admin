8b4ecf6ef6a0f94dec4513e722291150ed800140
GuoxiaoYi <1072992047@qq.com>
2024-07-01 23:32:33 +0800

    隐藏子活动

--- src/router/index.js
+++ src/router/index.js
@@ -2994,7 +2994,7 @@ export const constantRoutes = [
         name: 'ActivityChildren',
         component: () => import('@/views/activities/children'),
         meta: { title: '子活动管理', noCache: false, activeMenu: '/activities', buttons: [
-          { text: '新建子活动', action: 'create_children_activity', perms: ['child_activity_manage'] }
+          { text: '新建子活动', action: 'create_children_activity', perms: ['child_activity_manage_hide'] }
         ] },
         beforeEnter(to, from, next) {
           activityBeforeEnter(to, from, next)
