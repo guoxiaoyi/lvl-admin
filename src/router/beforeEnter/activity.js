f40099977e608dcff6eab056c3a40b9bb3871697
GuoxiaoYi <1072992047@qq.com>
2024-09-05 12:12:47 +0800

    防伪活动

--- src/router/beforeEnter/activity.js
+++ src/router/beforeEnter/activity.js
@@ -2,7 +2,6 @@ import store from '@/store'
 export const activityBeforeEnter = async(to, from, next) => {
   await store.dispatch('apiData/fetchData', { id: to.params.activityId }).then(data => {
   })
-  console.log(store.getters.activityData.type)
   next()
 }
 
