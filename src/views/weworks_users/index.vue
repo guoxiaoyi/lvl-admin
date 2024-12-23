c8a01cce87ddc5f1b23b4feaa1231c058951fe65
郭晓奕 <107992047@qq.com>
2024-12-23 22:52:36 +0800

    修改不可拉取员工时的提示

--- src/views/weworks_users/index.vue
+++ src/views/weworks_users/index.vue
@@ -28,7 +28,7 @@
         </div>
         <div class="panel panel-default table-responsive">
           <TotalPage>
-            <el-tooltip v-if="(result.departmentList && result.departmentList.length === 0) || (result.userList && result.userList.length === 0)" class="item" effect="light" content="应用可见范围为空，不能拉取数据" placement="top">
+            <el-tooltip v-if="(result.departmentList && result.departmentList.length === 0) && (result.userList && result.userList.length === 0)" class="item" effect="light" content="应用可见范围为空，不能拉取数据" placement="top">
               <el-button :disabled="true" type="success">
                 拉取信息
               </el-button>
